bf5f6c2f4c0557aa7d8f9b9975f533d41cd5820f
Jyoti Rajpal <jyoti.r@webvillee.in>
2023-07-05 13:13:14 +0530

    second commit

--- controller/test.controller.js
+++ controller/test.controller.js
@@ -2,7 +2,11 @@ greetings = function(req,res){
     res.json({status:200, success:true, message: 'Welcome to Webvillee'});
 };
 
+new_greetings = function(req,res){
+    res.json({status:200, success:true, message: 'Welcome to Webvillee, CI/CD pipeline'});
+};
 
 module.exports = {
     greetings, 
+    new_greetings
 }
--- router/my.router.js
+++ router/my.router.js
@@ -2,6 +2,7 @@ var router = require("express").Router();
 var testcontroller = require('../controller/test.controller');
 
 router.get('/greetings', testcontroller.greetings);
+router.get('/new_greetings', testcontroller.new_greetings);
 
 
 
